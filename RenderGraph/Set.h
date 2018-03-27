38293e5646c48163399f661d5670bffaa5c24da9
Arne Schober <arne.schober@gmx.net>
2018-03-27 16:21:21 -0400

    documentation

--- RenderGraph/Set.h
+++ RenderGraph/Set.h
@@ -3,13 +3,16 @@
 
 struct Set final
 {
+	/* Wrapper class to hande types that cannot be inherited from */
 	template<typename T>
 	struct SetElement
 	{};
-
+	
+	/* Type List of Elements each anotated with its index to be able to retrieve the element */
 	template<int Length, typename... TS>
 	struct SetElements;
 
+	/* The non empty List */
 	template<int Length, typename T, typename... TS>
 	struct SetElements<Length, T, TS...> : SetElements<Length+1, TS...>, SetElement<T&>
 	{
@@ -17,6 +20,7 @@ struct Set final
 		static constexpr int Index = Length;
 	};
 	
+	/* The empty List */
 	template<int Length>
 	struct SetElements<Length>
 	{
@@ -25,25 +29,32 @@ struct Set final
 
 	struct Internal
 	{
+		/* providing an I the compiler chooses the closest match during overload resultion */
+		/* as the I's are unique we can extract the type E the compiler will infer for us this way */ 
 		template<int I, typename E, typename... ES>
 		static constexpr auto GetTypeInternal(const SetElements<I, E, ES...>&)->E;
 	};
 
+	/* The type representing a compile time set */
 	template<typename... TS>
 	struct Type final : SetElements<0, TS...>
 	{
 		using BaseType = SetElements<0, TS...>;
+		
+		/* Does this Set contain this type? */
 		template<typename T>
 		static constexpr bool Contains()
 		{
 			return std::is_base_of_v<SetElement<T&>, Type<TS...>>;
 		}
 
+		/* Query the size of a Set */
 		static constexpr size_t GetSize() 
 		{ 
 			return sizeof...(TS); 
 		};
 		
+		/* Given an I what Type is stored at this Index */
 		template<int I>
 		static constexpr auto GetType() -> decltype(Internal::GetTypeInternal<I>(BaseType()))
 		{
@@ -52,33 +63,39 @@ struct Set final
 		}
 
 	private:
+		/* Use the compiler to check if each Type is Unique (Multiple inheritance from the same type is not allowed) */
 		struct UniqueElementChecker : SetElement<TS&>... {};
 	};
 
+	/* The Union of two Sets */
 	template<typename... XS, typename... YS>
 	static constexpr auto Union(const Type<XS...>&, const Type<YS...>&)
 	{
 		return Recurse<true>(Type<YS...>(), Type<XS...>(), Type<XS...>());
 	}
 
+	/* The Intersection of two Sets */
 	template<typename... XS, typename... YS>
 	static constexpr auto Intersect(const Type<XS...>&, const Type<YS...>&)
 	{
 		return Recurse<false>(Type<XS...>(), Type<YS...>(), Type<>());
 	}
 
+	/* The left Set without the right one */
 	template<typename... XS, typename... YS>
 	static constexpr auto LeftDifference(const Type<XS...>&, const Type<YS...>&)
 	{
 		return Recurse<true>(Type<XS...>(), Intersect(Type<XS...>(), Type<YS...>()), Type<>());
 	}
 
+	/* The right Set without the left one */
 	template<typename... XS, typename... YS>
 	static constexpr auto RightDifference(const Type<XS...>&, const Type<YS...>&)
 	{
 		return Recurse<true>(Type<YS...>(), Intersect(Type<XS...>(), Type<YS...>()), Type<>());
 	}
 
+	/* Two Sets combined without their Intersection */
 	template<typename... XS, typename... YS>
 	static constexpr auto Difference(const Type<XS...>&, const Type<YS...>&)
 	{
@@ -86,20 +103,24 @@ struct Set final
 	}
 
 private:
+	/* Recursively itterate through the first argument checking it against the second and accumulating the results in the third */
 	template<bool InverseTest, typename T, typename... TS, typename... XS, typename... RS>
 	static constexpr auto Recurse(const Type<T, TS...>&, const Type<XS...>&, const Type<RS...>&)
 	{
 		constexpr bool TestResult = Type<XS...>::template Contains<T>();
 		if constexpr (InverseTest ? !TestResult : TestResult)
 		{
+			//add the element T to the accumulation 
 			return Recurse<InverseTest>(Type<TS...>(), Type<XS...>(), Type<RS..., T>());
 		}
 		else
 		{
+			//skip over element T 
 			return Recurse<InverseTest>(Type<TS...>(), Type<XS...>(), Type<RS...>());
 		}
 	}
 
+	/* After itteration return the third parameter which contains the accumulated results */
 	template<bool InverseTest, typename... XS, typename... RS>
 	static constexpr auto Recurse(const Type<>&, const Type<XS...>&, const Type<RS...>&)
 	{
