4a76e5837d470a0519d2d8cb275873dbcc5a7f56
Khipu Kamayuq <alfatier@gmx.de>
2018-03-31 18:28:08 -0400

    Cons style has better compile time behaviour due to memonization

--- RenderGraph/Set.h
+++ RenderGraph/Set.h
@@ -30,28 +30,28 @@ struct Set final
 	template<typename... XS, typename... YS>
 	static constexpr auto Union(const Type<XS...>&, const Type<YS...>&)
 	{
-		return Recurse<true>(Type<YS...>(), Type<XS...>(), Type<XS...>());
+		return Recurse<true>(Type<YS...>(), Type<XS...>(), Type<XS...>(), Type<>());
 	}
 
 	/* The Intersection of two Sets */
 	template<typename... XS, typename... YS>
 	static constexpr auto Intersect(const Type<XS...>&, const Type<YS...>&)
 	{
-		return Recurse<false>(Type<XS...>(), Type<YS...>(), Type<>());
+		return Recurse<false>(Type<XS...>(), Type<YS...>(), Type<>(), Type<>());
 	}
 
 	/* The left Set without the right one */
 	template<typename... XS, typename... YS>
 	static constexpr auto LeftDifference(const Type<XS...>&, const Type<YS...>&)
 	{
-		return Recurse<true>(Type<XS...>(), Intersect(Type<XS...>(), Type<YS...>()), Type<>());
+		return Recurse<true>(Type<XS...>(), Intersect(Type<XS...>(), Type<YS...>()), Type<>(), Type<>());
 	}
 
 	/* The right Set without the left one */
 	template<typename... XS, typename... YS>
 	static constexpr auto RightDifference(const Type<XS...>&, const Type<YS...>&)
 	{
-		return Recurse<true>(Type<YS...>(), Intersect(Type<XS...>(), Type<YS...>()), Type<>());
+		return Recurse<true>(Type<YS...>(), Intersect(Type<XS...>(), Type<YS...>()), Type<>(), Type<>());
 	}
 
 	/* Two Sets combined without their Intersection */
@@ -63,26 +63,26 @@ struct Set final
 
 private:
 	/* Recursively itterate through the first argument checking it against the second and accumulating the results in the third */
-	template<bool InverseTest, typename T, typename... TS, typename... XS, typename... RS>
-	static constexpr auto Recurse(const Type<T, TS...>&, const Type<XS...>&, const Type<RS...>&)
+	template<bool InverseTest, typename T, typename... TS, typename... XS, typename... RS, typename... LS>
+	static constexpr auto Recurse(const Type<T, TS...>&, const Type<XS...>&, const Type<RS...>&, const Type<LS...>&)
 	{
 		constexpr bool TestResult = Type<XS...>::template Contains<T>();
 		if constexpr (InverseTest ? !TestResult : TestResult)
 		{
 			//add the element T to the accumulation 
-			return Recurse<InverseTest>(Type<TS...>(), Type<XS...>(), Type<RS..., T>());
+			return Recurse<InverseTest>(Type<TS...>(), Type<XS...>(), Type<RS...>(), Type<LS..., T>());
 		}
 		else
 		{
 			//skip over element T 
-			return Recurse<InverseTest>(Type<TS...>(), Type<XS...>(), Type<RS...>());
+			return Recurse<InverseTest>(Type<TS...>(), Type<XS...>(), Type<RS...>(), Type<LS...>());
 		}
 	}
 
 	/* After itteration return the third parameter which contains the accumulated results */
-	template<bool InverseTest, typename... XS, typename... RS>
-	static constexpr auto Recurse(const Type<>&, const Type<XS...>&, const Type<RS...>&)
+	template<bool InverseTest, typename... XS, typename... RS, typename... LS>
+	static constexpr auto Recurse(const Type<>&, const Type<XS...>&, const Type<RS...>&, const Type<LS...>&)
 	{
-		return Type<RS...>();
+		return Type<LS..., RS...>();
 	}
 };
