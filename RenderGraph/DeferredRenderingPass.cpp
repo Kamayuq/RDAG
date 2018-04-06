cdff41b39ca50fc0ebe315ea37fdd11d965e579f
Khipu Kamayuq <alfatier@gmx.de>
2018-04-06 01:13:44 -0400

    Better example that shows how resources are carried between seq operations: in this case the depth

--- RenderGraph/DeferredRenderingPass.cpp
+++ RenderGraph/DeferredRenderingPass.cpp
@@ -11,33 +11,38 @@
 
 typename DeferredRendererPass::PassOutputType DeferredRendererPass::Build(const RenderPassBuilder& Builder, const PassInputType& Input)
 {
+	using DeferredResult = ResourceTable
+	<
+		InputTable<RDAG::TransparencyInput>,
+		OutputTable<>
+	>;
 	using PostprocessingData = ResourceTable
 	<
-		InputTable<RDAG::SceneViewInfo, RDAG::DepthTexture>,
-		OutputTable<RDAG::TransparencyResult>
+		InputTable<RDAG::SceneViewInfo, RDAG::DepthTexture, RDAG::VelocityVectors, RDAG::TransparencyResult>,
+		OutputTable<>
 	>;
 	return Seq
 	{
-		Extract<PostprocessingData>(Seq
+		Builder.BuildRenderPass("DepthRenderPass", DepthRenderPass::Build),
+		Extract<DeferredResult>(Seq
 		{
-			Builder.BuildRenderPass("DepthRenderPass", DepthRenderPass::Build),
 			Builder.BuildRenderPass("GbufferRenderPass", GbufferRenderPass::Build),
 			Builder.BuildRenderPass("AmbientOcclusionPass", AmbientOcclusionPass::Build),
 			Builder.BuildRenderPass("ShadowMapRenderPass", ShadowMapRenderPass::Build),
 			Builder.BuildRenderPass("DeferredLightingPass", DeferredLightingPass::Build),
 			Builder.RenameOutputToInput<RDAG::LightingUAV, RDAG::TransparencyInput>(),
-			Builder.BuildRenderPass("TransparencyRenderPass", TransparencyRenderPass::Build),
 		}),
+		Builder.BuildRenderPass("TransparencyRenderPass", TransparencyRenderPass::Build),
+		Builder.BuildRenderPass("VelocityRenderPass", VelocityRenderPass::Build),
 		Select<PostprocessingData>(Seq
 		{
-			Builder.BuildRenderPass("VelocityRenderPass", VelocityRenderPass::Build),
 			Scope(Seq
 			{
-				Builder.RenameOutputToInput<RDAG::TransparencyResult, RDAG::TemporalAAInput>(),
+				Builder.RenameInputToInput<RDAG::TransparencyResult, RDAG::TemporalAAInput>(),
 				Builder.BuildRenderPass("TemporalAARenderPass", TemporalAARenderPass::Build),
 				Builder.RenameOutputToInput<RDAG::TemporalAAOutput, RDAG::TransparencyResult>()
 			}),
-			Builder.RenameOutputToInput<RDAG::TransparencyResult, RDAG::PostProcessingInput>(),
+			Builder.RenameInputToInput<RDAG::TransparencyResult, RDAG::PostProcessingInput>(),
 			Builder.BuildRenderPass("PostProcessingPass", PostProcessingPass::Build)
 		})
 	}(Input);
